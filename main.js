2f52409e30ad074d8eff602c819e7a0b381c41ac
TheBamBam189 <41122554+TheBamBam189@users.noreply.github.com>
2019-01-15 14:14:13 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const RhythmBot = require('./src/rhythm-bot.js');
+const logger = require('./src/logger.js');
+const bot = new RhythmBot({
+    command: {
+        symbol: '-' // command symbol trigger
+    },
+    discord: {
+        token: 'NDY0ODE4NDMyMDA1ODk4MjYw.Dq5Wbw.k9oPmHxlXW-PjV5akUFUDKMqGiY'//,
+        // manage: {
+        //     channels: [ 
+        //         // Example text channel manager, limits media channel to 5 posts, limit should probably be less than 100 to avoid bugs
+        //         { name: 'media', limit: 5 }  
+        //     ]
+        // }
+    }
+});
+
+bot.connect()
+    .then(() => { 
+        logger.log('Listening');
+        bot.listen();
+    })
+    .catch(err => {
+        logger.error(err);
+    });
